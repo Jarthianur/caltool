fc041cf8d45d51aaa2f8d8359ac44143c030c215
Timo Bruderek <timo.bruderek@gmx.de>
2015-01-16 05:23:21 +0000

    Added scripts to call caltool

---
+++ calibrate_landscape.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# copy zero cal to udev
+cp touchscreen.rules.template /etc/udev/rules.d/touchscreen.rules
+
+#udpate rules to zero calibration
+udevadm control --reload-rules
+udevadm trigger
+
+# call caltool
+./caltool -r landscape
+
+#copy new values to udev
+cp touchscreen.rules /etc/udev/rules.d/
+
+#udpate rules to new calibration
+udevadm control --reload-rules
+udevadm trigger
